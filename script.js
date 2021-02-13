beaf27524fa2e1b80def959dffad1e8e4d46b6b1
Eduardo Fonseca <58578495+Eduvsky@users.noreply.github.com>
2021-02-13 03:51:01 -0300

    Updating the main script

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ function gettime(){
     var minute = date.getMinutes();
     var second = date.getSeconds();
 
-   
+    if (hour < 10){
+        hour = "0" + hour
+    }
+    
+    if (minute < 10){
+        minute = "0" + minute
+    }
 
+    if (second < 10){
+        second = "0" + second
+    }
     var time = hour + ":" + minute + ":" + second; 
     document.getElementById("main").innerText = time;
     document.getElementById("main").textContent = time;
